909fd74907009f20008d1e16ab35653a750d33ec
yamanidev <elamine.yamani@gmail.com>
2022-01-07 22:48:42 +0100

    Defined project structure

---
+++ test/helper/isString.test.js
@@ -0,0 +1,22 @@
+import isString from "../../src/helper/isString";
+
+it("should return false for non-string types", () => {
+
+    const input = [
+        10,
+        [],
+        ["someArray"],
+        {},
+        {
+            name: "someObject"
+        },
+        null,
+        undefined,
+        true,
+        false
+    ];
+
+    input.forEach(item => {
+        expect(isString(item)).toBe(false);
+    })
+});
